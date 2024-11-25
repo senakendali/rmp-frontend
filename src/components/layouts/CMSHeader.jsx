b6323846224c46b13560d0cafd88b1bd1b797425
ramanandhaaja <thisisnandha@gmail.com>
2024-11-26 00:58:14 +0700

    Update CMSHeader.jsx

--- src/components/layouts/CMSHeader.jsx
+++ src/components/layouts/CMSHeader.jsx
@@ -9,7 +9,7 @@ const CMSHeader = () => {
         <div className="flex items-center space-x-4">
           <div className="flex items-center">
             <img
-              src="./public/RMPLogo.png"
+              src="/RMPlogo.png"
               alt="Royal Medica logo"
               className="h-8"
             />
