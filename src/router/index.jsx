c2bcc6c0c76215c5feb9d685dc17d009e0dcb80b
ihsan <karamiihsan@gmail.com>
2024-12-05 11:39:03 +0700

    add vendor management menu on side bar nav

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import RNDPages from "../pages/RNDPages";
 import TestPage from "../pages/TestPage";
+import VendorManagement from "../pages/vendorManagement/index";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
   {
     path: "/test",
     element: <TestPage />,
+  },
+  {
+    path: "/vendor-management/vendor-list",
+    element: <VendorManagement />,
   },
 ]);
 
